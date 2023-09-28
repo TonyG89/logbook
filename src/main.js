5a8b4f8119c6aa4fffcec4600ab9b90fc126345a
Tony-tuf15 <a.gryhoryev@gmail.com>
2023-09-28 19:17:11 +0300

    first

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+
+import App from './App.vue'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App)
+
+app
+  .use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    /*
+    config: {
+      brand: {
+        // primary: '#e46262',
+        // ... or all other brand colors
+      },
+      notify: {...}, // default set of options for Notify Quasar plugin
+      loading: {...}, // default set of options for Loading Quasar plugin
+      loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+      // ..and many more (check Installation card on each Quasar component/directive/plugin)
+    }
+    */
+  })
+  .mount('#app')
