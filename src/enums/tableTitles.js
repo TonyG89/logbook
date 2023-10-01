a1897297659e5f7b18c44182f35b040e628d55d0
Tony-tuf15 <a.gryhoryev@gmail.com>
2023-10-01 17:39:15 +0300

    hashtables

---
+++ src/enums/tableTitles.js
@@ -0,0 +1,35 @@
+export default {
+  // 'id':'',
+  date:{
+    title: 'Дата',
+    value: 'date',
+  },
+  kilometers:{
+    title: 'Километры',
+    value: 'kilometers',
+  },
+  categories:{
+    title: 'Категории',
+    value: 'categories',
+  },
+  actions:{
+    title: 'Действия',
+    value: 'actions',
+  },
+  status:{
+    title: 'Статус',
+    value: 'status',
+  },
+  details:{
+    title: 'Детали',
+    value: 'details',
+  },
+  work:{
+    title: 'Работа',
+    value: 'work',
+  },
+  comments:{
+    title: 'Комментарии',
+    value: 'comments',
+  }
+};
